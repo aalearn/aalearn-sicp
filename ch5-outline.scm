7064d07f36762f5324f223cad1d01ea87ed6dae6
aalearn <anshul.amar@gmail.com>
2010-05-09 20:07:13 -0400

    partway through 5.35

--- ch5-outline.scm
+++ ch5-outline.scm
@@ -991,25 +991,11 @@ ev-cond-finish
 
 ;;  * _ Exercise 5.25
 
-ev-delayed
-  (assign exp (op delayed-body) (reg exp)) ; delayed-body, env not defined (number of ways to do it)
-  (save env)
-  (assign env (op delayed-env) (reg env))  ; needed?
-  (save continue)
-  (assign continue (label ev-delayed-finish))
-  (goto (label eval-dispatch))
-
-ev-delayed-finish
-  (restore continue)
-  (restore env)
-
-ev-delay
-  (assign val (op make-delayed) (reg exp) (reg env))  ; make-delayed not defined
-  (goto (reg continue))
-
-; still unclear when things should be forced
-; not finished - skipping!
-
+; added make-thunk and related functions to support
+; primitive procs are strict
+; self-evaluating are immediately evaluated
+; printing forces thunks
+; see commit: 7a9aab73f8ce562ce12cf4f2fec4adaad0d00b7b
 
 ;;  * _ Exercise 5.26
 ; iterative
@@ -1132,19 +1118,191 @@ ev-delay
 
 ((f) 'x 'y)
 ; all are superfluous except env around the operator
+; NB: why don't we save/restore argl around the eval of the operator?
 
 (f (g 'x) y)
 ; superfluous:
 ;  argl and proc do not need to be saved around eval of y or 'x
-;  env did not need to be saved anywhere
+;  env does not need to be saved anywhere
 
 (f (g 'x) 'y)
 ; why is this possibly different than the above?
 
 ;;  * _ Exercise 5.32
-; we were previously saving the following:
 
+; a. see commit: 06c3dd5cccfe51bdf8b6fd4a1cd9b1bae0d5fd3f
 ; b. Introducing special cases will make the evaluator more efficient than it was,
 ;  but there is a cost to checking all the special cases, and this cost is incurred
 ;  every time an expression is analyzed.  The compiler will incur this cost only once
 ;  at compile time, which will therefore still give it an advantage.
+
+
+;;  * _ Exercise 5.33
+(compile
+  '(define (factorial n)
+     (if (= n 1)
+	 1
+	 (* (factorial (- n 1)) n)))
+  'val
+  'next)
+
+;; construct the procedure and skip over code for the procedure body
+  (assign val
+          (op make-compiled-procedure) (label entry2) (reg env))
+  (goto (label after-lambda1))
+
+entry2     ; calls to factorial will enter here
+  (assign env (op compiled-procedure-env) (reg proc))
+  (assign env
+          (op extend-environment) (const (n)) (reg argl) (reg env))
+;; begin actual procedure body
+  (save continue)
+  (save env)
+
+;; compute (= n 1)
+  (assign proc (op lookup-variable-value) (const =) (reg env))
+  (assign val (const 1))
+  (assign argl (op list) (reg val))
+  (assign val (op lookup-variable-value) (const n) (reg env))
+  (assign argl (op cons) (reg val) (reg argl))
+  (test (op primitive-procedure?) (reg proc))
+  (branch (label primitive-branch17))
+compiled-branch16
+  (assign continue (label after-call15))
+  (assign val (op compiled-procedure-entry) (reg proc))
+  (goto (reg val))
+primitive-branch17
+  (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+
+after-call15   ; val now contains result of (= n 1)
+  (restore env)
+  (restore continue)
+  (test (op false?) (reg val))
+  (branch (label false-branch4))
+true-branch5  ; return 1
+  (assign val (const 1))
+  (goto (reg continue))
+
+false-branch4
+;; compute and return (* (factorial (- n 1)) n)
+  (assign proc (op lookup-variable-value) (const *) (reg env))
+  (save continue)
+  (save proc)   ; save * procedure
+  (assign val (op lookup-variable-value) (const n) (reg env))
+  (assign argl (op list) (reg val))
+  (save argl)   ; save partial argument list for *
+
+;; compute (factorial (- n 1)), which is the other argument for *
+  (assign proc
+          (op lookup-variable-value) (const factorial) (reg env))
+  (save proc)  ; save factorial procedure
+
+
+(compile
+ '(define (factorial-alt n)
+  (if (= n 1)
+      1
+      (* n (factorial-alt (- n 1)))))
+ 'val
+ 'next)
+
+((env) (val) 
+ (
+  (assign val (op make-compiled-procedure) (label entry19) (reg env)) 
+  (goto (label after-lambda18))  
+entry19 
+  (assign env (op compiled-procedure-env) (reg proc)) 
+  (assign env (op extend-environment) (const (n)) (reg argl) (reg env)) 
+
+;; begin actual procedure body
+  (save continue) 
+  (save env) 
+
+;; compute (= n 1)
+  (assign proc (op lookup-variable-value) (const =) (reg env)) 
+  (assign val (const 1)) 
+  (assign argl (op list) (reg val)) 
+  (assign val (op lookup-variable-value) (const n) (reg env)) 
+  (assign argl (op cons) (reg val) (reg argl)) 
+  (test (op primitive-procedure?) (reg proc)) 
+  (branch (label primitive-branch34)) 
+compiled-branch33 
+  (assign continue (label after-call32)) 
+  (assign val (op compiled-procedure-entry) (reg proc)) 
+  (goto (reg val)) 
+primitive-branch34 
+  (assign val (op apply-primitive-procedure) (reg proc) (reg argl)) 
+after-call32 
+  (restore env) 
+  (restore continue) 
+  (test (op false?) (reg val)) 
+  (branch (label false-branch21)) 
+true-branch22 
+  (assign val (const 1)) (goto (reg continue)) 
+false-branch21 
+;; compute and return (* n (factorial-alt (- n 1)))
+  (assign proc (op lookup-variable-value) (const *) (reg env)) 
+  (save continue) 
+  (save proc) 
+;; ** env is saved here
+  (save env) 
+;; ** The factorial-alt is looked up before getting n
+  (assign proc (op lookup-variable-value) (const factorial-alt) (reg env)) 
+  (save proc) 
+  (assign proc (op lookup-variable-value) (const -) (reg env)) 
+  (assign val (const 1)) 
+  (assign argl (op list) (reg val)) 
+  (assign val (op lookup-variable-value) (const n) (reg env)) 
+  (assign argl (op cons) (reg val) (reg argl)) 
+  (test (op primitive-procedure?) (reg proc)) 
+  (branch (label primitive-branch25)) 
+compiled-branch24 
+  (assign continue (label after-call23)) 
+  (assign val (op compiled-procedure-entry) (reg proc))
+  (goto (reg val)) 
+primitive-branch25 
+  (assign val (op apply-primitive-procedure) (reg proc) (reg argl)) 
+after-call23 
+  (assign argl (op list) (reg val)) 
+  (restore proc) 
+  (test (op primitive-procedure?) (reg proc)) 
+  (branch (label primitive-branch28)) 
+compiled-branch27 
+  (assign continue (label after-call26)) 
+;; factorial-alt called here
+  (assign val (op compiled-procedure-entry) (reg proc)) 
+  (goto (reg val)) 
+primitive-branch28 
+ (assign val (op apply-primitive-procedure) (reg proc) (reg argl)) 
+after-call26 
+  (assign argl (op list) (reg val)) 
+;; ** env is restored here
+  (restore env) 
+  (assign val (op lookup-variable-value) (const n) (reg env)) 
+  (assign argl (op cons) (reg val) (reg argl)) 
+  (restore proc) 
+  (restore continue) 
+  (test (op primitive-procedure?) (reg proc)) 
+  (branch (label primitive-branch31)) 
+compiled-branch30 
+  (assign val (op compiled-procedure-entry) (reg proc)) 
+  (goto (reg val)) 
+primitive-branch31 
+  (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+  (goto (reg continue)) 
+after-call29 
+after-if20 
+after-lambda18 
+  (perform (op define-variable!) (const factorial-alt) (reg val) (reg env)) 
+  (assign val (const ok))
+))
+
+; It appears that this version does not actually use tail call optimization --
+; we will get extra envs stored on the stack for each recursion.  Marked above.
+; This happens because the compiler can detect the earlier call
+
+
+;;  * _ Exercise 5.34
+
+;;  * _ Exercise 5.35
+(define (f x) (+ x (g (+ x 2))))
